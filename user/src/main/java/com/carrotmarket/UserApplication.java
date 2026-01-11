83f4a314b2b353002460457d31a03044eb7a4ac0
limheesang <dlagmltkd12@naver.com>
2026-01-11 20:45:34 +0900

    Initial commit

---
+++ user/src/main/java/com/carrotmarket/UserApplication.java
@@ -0,0 +1,13 @@
+package com.carrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
