4c8be29542108ea1470031c2519aafd63b8808c6
limheesang <dlagmltkd12@naver.com>
2026-02-09 01:35:11 +0900

    chore: Product 서브모듈 생성
    #5

---
+++ Product/src/main/java/com/carrotmarket/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.carrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+
+}
